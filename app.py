dacf9a2354ab849690c0ccb587b8bb7e42be8d1a
Nikolay <137264272+nymedvedev@users.noreply.github.com>
2024-05-06 19:40:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,110 @@
+import json
+from collections import defaultdict
+from flask import Flask, request, jsonify
+from pytrie import Trie
+import uuid
+import requests
+import time
+import re
+
+app = Flask(__name__)
+
+data = list()
+country_index = defaultdict(list)
+name_index = dict()
+domain_index = defaultdict(list)
+
+# Time to wait before allowing an update to our dataset. 86400 seconds = 24 hours
+UPDATE_WAIT_TIME = 86400
+
+@app.route("/search")
+def search():
+    if not data_loaded:
+        load_data()
+
+    country = request.args.get('country')
+    name = request.args.get('name')
+    name_contains = request.args.get('name_contains')
+    domain = request.args.get("domain")
+    filtered = data
+    limit = request.args.get('limit')
+    offset = request.args.get('offset')
+
+    if name and country:
+        name = name.lower()
+        country = country.lower()
+        name_filtered = prefix_tree.values(prefix=name)
+        filtered = [uni for uni in name_filtered if uni['country'].lower() == country]
+    elif name_contains and country:
+        country = country.lower()
+        regex = re.compile(r'\b{0}'.format(name_contains.lower()))
+        name_filtered = [uni for uni in data if regex.search(uni['name'].lower())]
+        filtered = [uni for uni in name_filtered if uni['country'].lower() == country]
+    elif name_contains:
+        regex = re.compile(r'\b{0}'.format(name_contains.lower()))
+        filtered = [uni for uni in data if regex.search(uni['name'].lower())]
+    elif name:
+        name = name.lower()
+        filtered = prefix_tree.values(prefix=name)
+    elif country:
+        country = country.lower()
+        filtered = country_index[country]
+    elif domain:
+        filtered = domain_index[domain]
+
+    if offset:
+        offset = int(offset)
+        if offset > len(filtered):
+            filtered = []
+        else:
+            filtered = filtered[offset:]
+    if limit:
+        limit = int(limit)
+        filtered = filtered[:limit]
+    return jsonify(filtered)
+
+data_loaded = False
+last_updated = 0
+
+def load_data():
+    global data_loaded, prefix_tree, data, country_index, name_index, domain_index
+    response = requests.get("https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json")
+    data = response.json()
+    for i in data:
+        country_index[i["country"].lower()].append(i)
+        name_index[i['name'].lower()] = i
+        for domain in i["domains"]:
+            domain_index[domain].append(i)
+        splitted = i['name'].split(" ")
+        if len(splitted) > 1:
+            for splitted_name in splitted[1:]:
+                name_index[splitted_name.lower() + str(uuid.uuid1())] = i
+    prefix_tree = Trie(**name_index)
+
+    data_loaded = True
+
+@app.route('/update')
+def update():
+    global last_updated
+
+    if (time.time() >= last_updated + UPDATE_WAIT_TIME):
+        load_data()
+        last_updated = time.time()
+        response = {'status': 'success', 'message': 'Dataset updated!'}
+    else:
+        response = {'status': 'error', 'message': 'Dataset had been updated recently. Try again later.'}
+
+    return jsonify(response)
+
+@app.route('/')
+def index():
+    if not data_loaded:
+        load_data()
+
+    data = {'author': {'name': 'hipo', 'website': 'http://hipolabs.com'},
+            'example': 'http://universities.hipolabs.com/search?name=middle&country=Turkey',
+            'github': 'https://github.com/Hipo/university-domains-list'}
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run()
